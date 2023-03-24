5e57f57da64d7791c71869cf8218687d2243bf80
nathmcmi486 <124921103+nathmcmi486@users.noreply.github.com>
2023-03-24 14:53:39 -0400

    Update Form1.Designer.cs
    Manually added changes to ``Form1.Designer.cs``.

--- text_adventure_game/Form1.Designer.cs
+++ text_adventure_game/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace text_adventure_game
+namespace text_adventure_game
 {
     partial class Form1
     {
@@ -37,11 +37,19 @@
             this.option3 = new System.Windows.Forms.Button();
 
             this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(600, 60);
             this.outputLabel.Size = new System.Drawing.Size(350, 60);
             this.outputLabel.Text = "";
             this.outputLabel.TabIndex = 0;
             this.outputLabel.Location = new System.Drawing.Point(60, 30);
             this.outputLabel.ForeColor = System.Drawing.Color.LightGreen;
+
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(100, 60);
+            this.inputLabel.Text = "";
+            this.inputLabel.TabIndex = 0;
+            this.inputLabel.Location = new System.Drawing.Point(60, 180);
+            this.inputLabel.ForeColor = System.Drawing.Color.LightGreen;
 
             this.startButton.Location = new System.Drawing.Point(40, 30);
             this.startButton.Name = "startButton";
@@ -51,6 +59,7 @@
             this.startButton.ForeColor = System.Drawing.Color.LightGreen;
             this.startButton.Click += new System.EventHandler(this.startButton_click);
 
+            this.option1.Location = new System.Drawing.Point(60, 130);
             this.option1.Location = new System.Drawing.Point(350, 40);
             this.option1.Name = "option1";
             this.option1.Size = new System.Drawing.Size(100, 40);
@@ -59,6 +68,7 @@
             this.option1.ForeColor = System.Drawing.Color.LightGreen;
             this.option1.Click += new System.EventHandler(this.option1_click);
 
+            this.option2.Location = new System.Drawing.Point(170, 130);
             this.option2.Location = new System.Drawing.Point(460, 40);
             this.option2.Name = "option2";
             this.option2.Size = new System.Drawing.Size(100, 40);
@@ -67,6 +77,7 @@
             this.option2.ForeColor = System.Drawing.Color.LightGreen;
             this.option2.Click += new System.EventHandler(this.option2_click);
 
+            this.option3.Location = new System.Drawing.Point(280, 130);
             this.option3.Location = new System.Drawing.Point(570, 40);
             this.option3.Name = "option3";
             this.option3.Size = new System.Drawing.Size(100, 40);
@@ -79,7 +90,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Text adventure";
-            this.BackColor = System.Drawing.Color.Black;
+            this.BackColor = System.Drawing.Color.FromArgb(255, 30, 30, 30);
 
             this.Controls.Add(this.startButton);
         }
